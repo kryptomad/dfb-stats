ecb69cc3014721643ec8c8bb41aadadd4a33cb7f
Pierre Bußmann <pierre.bussmann@quinscape.de>
2025-07-24 23:26:36 +0200

    Angular setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,104 @@ import { Menubar } from 'primeng/menubar';
 import { MenuItem } from 'primeng/api';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgOptimizedImage } from '@angular/common';
-import {Button, ButtonModule} from 'primeng/button';
+import { Button, ButtonModule } from 'primeng/button';
+import { AppModule } from './app.module';
+import { Menu } from 'primeng/menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Menubar, NgOptimizedImage, ButtonModule],
+  imports: [RouterOutlet, Menubar, NgOptimizedImage, AppModule, Menu, Button],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'dfb-stats';
-  menubarItems: MenuItem[] = [
+  menuItems: MenuItem[] = [
+    {
+      label: 'Start',
+      items: [
+        {
+          label: 'Termin nächster Abend',
+          icon: 'pi pi-refresh',
+        },
+        {
+          label: 'Highlights letzter Spieltag',
+          icon: 'pi pi-upload',
+        },
+        {
+          label: 'Nächster Oskarabend',
+          icon: 'pi pi-upload',
+        },
+      ],
+    },
+    {
+      label: 'Spieltag',
+      items: [
+        {
+          label: 'Jahrestabelle',
+        },
+      ],
+    },
+    {
+      label: 'Spiele',
+      items: [],
+    },
+    {
+      label: 'Spieler',
+      items: [
+        {
+          label: 'Bestleistungen',
+        },
+        {
+          label: 'Spieler vs. Spieler',
+        },
+      ],
+    },
+    {
+      label: 'Statistiken',
+      items: [
+        {
+          label: 'Allzeit-Rekorde',
+        },
+        {
+          label: 'Jahresvergleich',
+        },
+        {
+          label: 'Trendverlauf',
+        },
+      ],
+    },
+    {
+      label: 'Rekorde',
+      items: [
+        {
+          label: 'Gesamtwertungen',
+        },
+        {
+          label: 'Jahreswertungen',
+        },
+        {
+          label: 'Spieltagswertungen',
+        },
+        {
+          label: 'Matchwertungen',
+        },
+      ],
+    },
+    {
+      label: 'Regelwerk',
+      items: [
+        {
+          label: 'Spielregeln',
+        },
+        {
+          label: 'Dartkasse',
+        },
+        {
+          label: 'Vereinsregeln',
+        },
+      ],
+    },
   ];
 }
