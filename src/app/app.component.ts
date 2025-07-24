0f880ea352ccadc54110238b19c7528900e644ba
Pierre Bußmann <pierre.bussmann@quinscape.de>
2025-07-24 22:58:41 +0200

    Angular setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Menubar } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgOptimizedImage } from '@angular/common';
+import {Button, ButtonModule} from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Menubar, NgOptimizedImage, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'dfb-stats';
+  menubarItems: MenuItem[] = [
+  ];
 }
