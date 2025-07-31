d6d52c5022f137cb4c1bdc0e47cc31ab45c6250d
kryptomad <kryptomad@proton.me>
2025-07-31 16:07:39 +0200

    ngFor changed to p-card tailwind decimal list used in fieldsete legend numbers added to titles

--- src/app/pages/rekorde/rekorde.component.ts
+++ src/app/pages/rekorde/rekorde.component.ts
@@ -20,7 +20,7 @@ export interface Rekord {
 })
 export class RekordeComponent {
   gesamtwertungen: Rekord[] = [
-    this.createRekord('Jahres-Oscar Gesamt', '2007–2016', 'Nico', 10),
+    this.createRekord('Jahres-Oscar Gesamt [3]', '2007–2016', 'Nico', 10),
     this.createRekord('Jahres-Oscar Gewinn in Folge', '2007–2016', 'Nico', 10),
     this.createRekord('Monats-Oscar Gesamt', '1996–2023/24', 'Y', 81),
     this.createRekord('Monats-Oscar Gewinn in Folge', '2019–2024', 'mad', 8),
@@ -35,7 +35,7 @@ export class RekordeComponent {
     ),
     this.createRekord('High Check', '2003, 2020/21, 2023/24', 'Y, mad', 9),
     this.createRekord(
-      'High Check geworfen (+100)',
+      'High Check geworfen [1] (+100)',
       '2008, 2013 / 2019, 2020/21',
       'Nico, mad',
       3,
@@ -44,23 +44,23 @@ export class RekordeComponent {
     this.createRekord('Geworfene 180er, Gesamt', '2008, 2013', 'Team', 8),
     this.createRekord('Geworfene 140er', '2021/2022', 'mad', 30),
     this.createRekord('Geworfene 140er, Gesamt', '2009, 2012', 'Team', 64),
-    this.createRekord('Geworfene TONs', '2021/2022', 'mad', 149),
-    this.createRekord('Punkte Jahreswertung', '2020/21', 'mad', 120),
-    this.createRekord('Spiele gewonnen', '2020/21, 2023/24', 'mad', 40),
+    this.createRekord('Geworfene TONs [8]', '2021/2022', 'mad', 149),
+    this.createRekord('Punkte Jahreswertung [7]', '2020/21', 'mad', 120),
+    this.createRekord('Spiele gewonnen [5]', '2020/21, 2023/24', 'mad', 40),
     this.createRekord('Spiele in Serie gewonnen', '2023/24', 'mad', 33),
-    this.createRekord('Short Games geworfen', '2023/24', 'mad', 17),
-    this.createRekord('Short Game, Anzahl Darts', '01.01.2007', 'Nico', 14),
+    this.createRekord('Short Games geworfen [2]', '2023/24', 'mad', 17),
+    this.createRekord('Short Game, Anzahl Darts [2]', '01.01.2007', 'Nico', 14),
   ];
 
   spieltagswertungen: Rekord[] = [
     this.createRekord('Geworfene 180er', '2006–2024', 'Y, Nico, mad', 2),
-    this.createRekord('Geworfene 180er, Gesamt', '02.02.2013', 'Team', 3),
+    this.createRekord('Geworfene 180er, Gesamt [2]', '02.02.2013', 'Team', 3),
     this.createRekord('Geworfene 140er', '13.04.2017', 'Nico', 8),
-    this.createRekord('Geworfene 140er, Gesamt', '2009–2017', 'Team', 12),
-    this.createRekord('Geworfene TONs', '21.12.2019', 'mad', 22),
-    this.createRekord('First 9 Dart Average', '2023/24', 'mad', 71.9),
-    this.createRekord('3 Dart Average', '2019', 'mad', 62.8),
-    this.createRekord('Average Darts needed', '2019', 'mad', 23.75),
+    this.createRekord('Geworfene 140er, Gesamt [2]', '2009–2017', 'Team', 12),
+    this.createRekord('Geworfene TONs [8]', '21.12.2019', 'mad', 22),
+    this.createRekord('First 9 Dart Average [6]', '2023/24', 'mad', 71.9),
+    this.createRekord('3 Dart Average [6]', '2019', 'mad', 62.8),
+    this.createRekord('Average Darts needed [6]', '2019', 'mad', 23.75),
   ];
 
   matchwertungen: Rekord[] = [
