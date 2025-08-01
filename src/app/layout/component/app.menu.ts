bfbb1d7ba7fb59afe278c4ae9a808807aac329ed
kryptomad <kryptomad@proton.me>
2025-08-01 11:57:10 +0200

    menu updated with spieler/spieler

--- src/app/layout/component/app.menu.ts
+++ src/app/layout/component/app.menu.ts
@@ -64,6 +64,11 @@ export class AppMenu implements OnInit {
       {
         label: 'Spieler',
         items: [
+                    {
+            label: 'Spieler',
+            icon: 'pi pi-fw pi-user',
+            routerLink: 'pages/spieler',
+          },
           {
             label: 'Bestleistungen',
             icon: 'pi pi-fw pi-bolt',
