0109bf2f0e6f63c36d13998e434002e6d12d8169
kryptomad <kryptomad@proton.me>
2025-08-05 10:11:20 +0200

    statstiken mit Allzeit-Rekorde gefüllt (Match) dafür stats.service.ts angelegt.

--- src/app/pages/statistiken/statistiken.component.ts
+++ src/app/pages/statistiken/statistiken.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, NgForOf, NgIf } from '@angular/common';
+import { Card } from 'primeng/card';
+
+import { StatsService } from '../../services/stats.service';
 
 @Component({
   selector: 'app-statistiken',
-  imports: [],
+  imports: [CommonModule, NgForOf, NgIf, Card],
+  providers: [StatsService],
   templateUrl: './statistiken.component.html',
-  styleUrl: './statistiken.component.scss'
+  styleUrl: './statistiken.component.scss',
 })
-export class StatistikenComponent {
+export class StatistikenComponent implements OnInit {
+  constructor(private statsService: StatsService) {}
 
+  bestLegs: any[] = [];
+  highestCheckout: any[] = [];
+  best3DA: any[] = [];
+  bestFirst9: any[] = [];
+  mostTONs: any[] = [];
+  most140s: any[] = [];
+  most180s: any[] = [];
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.bestLegs = this.statsService.getBestLegMatch();
+      this.highestCheckout = this.statsService.getHighestCheckoutMatch();
+      this.best3DA = this.statsService.getBest3DAMatch();
+      this.bestFirst9 = this.statsService.getBestFirst9Match();
+      this.mostTONs = this.statsService.getMostTONsMatch();
+      this.most140s = this.statsService.getMost140sMatch();
+      this.most180s = this.statsService.getMost180sMatch();
+    }, 400);
+  }
 }
